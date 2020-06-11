a5205834165f9d3cd38238782be1d5f133226cb5
Anupam <onlyprogramming000@gmail.com>
2020-06-11 16:16:06 +0530

    Bit magic

---
+++ snippet.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie();                        \
+    cout.tie();
+
+int main()
+{
+    FAST return 0;
+}
