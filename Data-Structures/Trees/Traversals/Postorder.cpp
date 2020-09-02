80d6d1ce0ca8eb1d70493c4374e26a190a5b1378
Anupam <anusrivastava910@gmail.com>
2020-09-02 11:22:36 +0530

    Depth First Order

---
+++ Data-Structures/Trees/Traversals/Postorder.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left, *right;
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+void inorder(Node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        inorder(root->right);
+        cout << root->data << " ";
+    }
+}
+
+int main()
+{
+    Node *root = new Node(50);
+    root->left = new Node(40);
+    root->right = new Node(60);
+    root->left->left = new Node(20);
+    root->left->right = new Node(30);
+    root->right->left = new Node(30);
+    root->right->right = new Node(40);
+    inorder(root);
+    return 0;
+}
